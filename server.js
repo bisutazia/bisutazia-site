9c883936e130128401c6765ec1716d0b7e7cb1e2
bisutazia <yuya.hapinesu@outlook.jp>
2025-04-17 23:23:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -189,7 +189,7 @@ app.get('/history', (req, res) => {
 });
 
 
-const serviceAccount = require('./firebase-service-account.json');
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
